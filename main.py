0630eaf43fddb97a99884e049c5e414da3735f0b
juligric <49377944@est.ort.edu.ar>
2025-04-08 15:50:58 -0300

    tablero con barcos termiandos

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import random 
 N = 10  # Podés cambiar este número a lo que quieras
+cantidadDeBarcos = 10
+intentos = 15
 
 # Creamos el tablero vacío
 tablero = []
 
 # Repetimos N veces para crear N filas
 for i in range(N):
-    fila = []  # Creamos una fila vacía
-    for j in range(N):  # En cada fila, agregamos N columnas
-        fila.append(0)  # Agregamos un 0 como columna vacía (sin barco)
-    tablero.append(fila)  # Agregamos la fila completa al tablero
+    fila = []
+    for j in range(N):
+        fila.append(0)
+    tablero.append(fila)
+
+# Colocamos barcos al azar
+barcosColocados = 0
+while barcosColocados < cantidadDeBarcos:
+    fila = random.randint(0, N - 1)
+    col = random.randint(0, N - 1)
+    if tablero[fila][col] == 0:
+        tablero[fila][col] = 1  # Poner un barco
+        barcosColocados += 1  # ¡Sumar uno al contador!
+
+
+
+
+
+
+
 
 # Mostramos el tablero
 for fila in tablero:
